56b6a82a42e5fa14ee4206903b399e31232e1425
Hisban Shiraz <hisbanbardai@gmail.com>
2024-09-10 11:00:33 -0600

    Implement functionality to redirect to the home page when the logo button is clicked

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,6 +1,11 @@
 import "./Logo.css";
 import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 export default function Logo() {
-  return <img className="logo-image" src={logo} alt="" />;
+  return (
+    <Link to={"/"}>
+      <img className="logo-image" src={logo} alt="" />
+    </Link>
+  );
 }
